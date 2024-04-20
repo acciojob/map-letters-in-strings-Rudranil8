1c8de9f427fac667bdc3564708463e415b779143
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-04-21 02:34:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function mapLetters(string){
+	let letterMap={};
+	for(let i=0; i<string.length; i++){
+		let char=string[i];
+		if(letterMap[char]){
+			letterMap[char].push(i);
+		}
+		else
+		{
+			letterMap[char]=[i];
+		}
+	}
+	return letterMap;
+}
